a90c8e73e17eeaa967ced713bac19023a4caf60f
Tigran <soghomonyantigran@gmail.com>
2019-08-09 02:54:57 +0400

    Delete, Addfunctionalities are ready.

---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./item-add-form.css";
+
+export default class ItemAddForm extends Component {
+  render() {
+    const { onItemAdded } = this.props;
+    return (
+      //   <form>
+      //     <input type="text" />
+      //     <button>Add Item</button>
+      //   </form>
+      <div className="item-add-form">
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => onItemAdded("Hello!")}
+        >
+          Add Item
+        </button>
+      </div>
+    );
+  }
+}
